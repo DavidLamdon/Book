66e2bffd73b18eff03c2d8784b77e94e9e939efd
DavidLamdon <vladag770@gmail.com>
2023-12-12 21:15:48 +0200

    Add project files.

---
+++ Book/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp
+{
+    public class Book
+    {
+        private String name;
+        private int maxPage;
+        private int currentPage;
+
+        public Book(String bookName, int max) 
+        {
+            name = bookName;
+            maxPage = max;
+        }
+
+        public int GetCurrentPage()
+        {
+            return currentPage;
+        }
+
+        public void SetCurrentPage(int curPage)
+        {
+            currentPage = curPage;
+        }
+        public void Read()
+        {
+            currentPage++;
+        }
+        public void Read(int pages)
+        {
+            currentPage += pages;
+        }
+        public bool IsFinished()
+        {
+            return currentPage == maxPage;
+        }
+        public int GetPagesTillTheEnd()
+        {
+            return maxPage - currentPage;
+        }
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bk = new Book("Oliver", 50);
+            Console.WriteLine(bk.GetCurrentPage());
+            bk.SetCurrentPage(49);
+            Console.WriteLine(bk.GetPagesTillTheEnd());
+            bk.Read();
+            Console.WriteLine(bk.IsFinished());
+        }
+    }
+
+}
